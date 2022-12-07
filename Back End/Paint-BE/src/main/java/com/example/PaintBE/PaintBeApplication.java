a97bcb3928b18875abd6e6ada1dfa4a386c2697f
MariamAziz0 <ma27523@gmail.com>
2022-12-08 00:35:47 +0200

    BackEnd

---
+++ Back End/Paint-BE/src/main/java/com/example/PaintBE/PaintBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaintBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBeApplication.class, args);
+	}
+
+}
---
+++ Back End/Paint-BE/src/test/java/com/example/PaintBE/PaintBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaintBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
